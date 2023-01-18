4c7d4d949d276abdf54c289617e6bbdcb8a19109
gabrielsantiag0 <gabrielleandrosantiag09@gmail.com>
2023-01-18 16:24:54 -0300

    projeto

--- conn/connect.php
+++ conn/connect.php
@@ -1,1 +1,17 @@
-<?php ?>
+<?php 
+
+$host = "localhost";
+$database = "ti93phpdb01";
+$user = "root";
+$pass = "";
+$charset = "utf8";
+$port = "3306";
+
+$conn = new mysqli($host, $user,$pass,$database,$port);
+mysqli_set_charset($conn, $charset);
+
+if ($conn->connect_error){
+        echo "Atenção ERRO: ".$conn->connect_error;
+}
+
+?>
