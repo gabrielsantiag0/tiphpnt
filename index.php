2e4799c961642fad7f10d954b780e992577f48b6
gabrielsantiag0 <gabrielleandrosantiag09@gmail.com>
2023-01-19 17:11:40 -0300

    site

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="./css/estilo.css">
     <title>Churrascaria fogo de chão</title>
 </head>
 <body class="fundofixo">
@@ -28,4 +28,19 @@
     </footer>
     </main>
 </body>
+<!-- link para bootstrap -->
+<script src="https://ajax.googleapis.com/ajax;libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min-js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).on('ready',function(){
+        $(".regular").slick({
+            dots:true,
+            Infinity: true, 
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+    });
+</script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick/slick.min.js"></script>
 </html>
---
+++ produtos_busca.php
@@ -0,0 +1,17 @@
+<?php 
+    include "conn/connect.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Buscar por palavras</title>
+</head>
+<body class="">
+    <?php include "menu_publico.php"; ?>    
+
+</body>
+</html>
